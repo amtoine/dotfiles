b843d615b67094082d78a1dda57621d969e1105b
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2021-11-13 16:43:47 +0100

    Color palette + battery in bar.

--- scripts/spectrWM-baraction.sh
+++ scripts/spectrWM-baraction.sh
@@ -18,164 +18,176 @@ hostname="${HOSTNAME:-${hostname:-$(hostname)}}"
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 hddicon() {
-	echo "HDD"
+  echo "HDD"
 }
 hdd() {
-	free="$(df -h /home | grep /dev | awk '{print $3}' | sed 's/G/Gb/')"
-	perc="$(df -h /home | grep /dev/ | awk '{print $5}')"
-	echo "$perc  ($free)"
+  free="$(df -h /home | grep /dev | awk '{print $3}' | sed 's/G/Gb/')"
+  perc="$(df -h /home | grep /dev/ | awk '{print $5}')"
+  echo "$perc  ($free)"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #              RAM
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 memicon() {
-	echo "MEM"
+  echo "MEM"
 }
 
 mem() {
-	used="$(free | grep Mem: | awk '{print $3}')"
-	total="$(free | grep Mem: | awk '{print $2}')"
-	human="$(free -h | grep Mem: | awk '{print $3}' | sed s/i//g)"
-	ram="$(( 200 * $used/$total - 100 * $used/$total ))% ($human) "
-	echo "$ram"
+  used="$(free | grep Mem: | awk '{print $3}')"
+  total="$(free | grep Mem: | awk '{print $2}')"
+  human="$(free -h | grep Mem: | awk '{print $3}' | sed s/i//g)"
+  ram="$(( 200 * $used/$total - 100 * $used/$total ))% ($human) "
+  echo "$ram"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #              CPU
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 cpuicon() {
-	echo "CPU"
+  echo "CPU"
 }
 cpu() {
-	echo "$(mpstat | tail -n 1 | awk '{print $3}')%"
+  echo "$(mpstat | tail -n 1 | awk '{print $3}')%"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #            VOLUME
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 volicon() {
-	echo "VOL"
+  echo "VOL"
 }
 vol() {
-	echo ""
+  echo ""
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #          PACKAGES
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 pkgicon() {
-	echo "PKGs"
+  echo "PKGs"
 }
 pkgs() {
-	pkgs=$(pacman -Qq | wc -l)
-	echo "$pkgs"
+  pkgs=$(pacman -Qq | wc -l)
+  echo "$pkgs"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #          REMOVABLE
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 remicon() {
-	echo "REM"
+  echo "REM"
 }
 rem() {
-	rems=$(pacman -Qdt | wc -l)
-	echo "$rems"
+  rems=$(pacman -Qdt | wc -l)
+  echo "$rems"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #           NETWORK
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 networkicon() {
-	wire="$(ip a | grep "eth" | grep inet | wc -l)"
-	wifi="$(ip a | grep "wl" | grep inet | wc -l)"
+  wire="$(ip a | grep "eth" | grep inet | wc -l)"
+  wifi="$(ip a | grep "wl" | grep inet | wc -l)"
 
-	if [ $wire = 1 ]; then
-		echo "WIRE"
-	elif [ $wifi = 1 ]; then
-		echo "WIFI"
-	else
-		echo "OFF"
-	fi
+  if [ $wire = 1 ]; then
+  	echo "WIRE"
+  elif [ $wifi = 1 ]; then
+  	echo "WIFI"
+  else
+  	echo "OFF"
+  fi
 }
 connection() {
-	echo "$(nmcli d | grep "\<connected\>" | awk '{ print substr($0, index($0,$4)) }')"
+  echo "$(nmcli d | grep "\<connected\>" | awk '{ print substr($0, index($0,$4)) }')"
 }
 ipaddress() {
-	address="$(ip a | grep .255 | grep -v wlp | cut -d ' ' -f6 | sed 's/\/24//')"
-	echo "$address"
+  address="$(ip a | grep .255 | grep -v wlp | cut -d ' ' -f6 | sed 's/\/24//')"
+  echo "$address"
 }
 vpnconnection() {
-	state="$(ip a | grep tun0 | grep inet | wc -l)"
+  state="$(ip a | grep tun0 | grep inet | wc -l)"
 
-	if [ $state = 1 ]; then
-		echo "VPN"
-	else
-		echo "NO VPN"
-	fi
+  if [ $state = 1 ]; then
+  	echo "VPN"
+  else
+  	echo "NO VPN"
+  fi
 }
 upicon(){ 
-	echo "TX" 
+  echo "TX" 
 }
 downicon(){
-	echo "RX"
+  echo "RX"
 }
 up(){
-	ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $9}'
+  ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $9}'
 }
 down(){
-	ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $7}'
+  ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $7}'
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #           ENTROPY
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 entropyicon() {
-	echo "ENT"
+  echo "ENT"
 }
 entropy(){
-	echo "$(cat /proc/sys/kernel/random/entropy_avail)"
+  echo "$(cat /proc/sys/kernel/random/entropy_avail)"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #           BATTERY
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-bat() {
-	batstat="$(cat /sys/class/power_supply/BAT0/status)"
-	battery="$(cat /sys/class/power_supply/BAT0/capacity)"
-echo "$batstat  $battery %"
+batstat() {
+  stat="$(cat /sys/class/power_supply/BAT0/status)"
+  echo "$stat"
+}
+battery() {
+  battery="$(cat /sys/class/power_supply/BAT0/capacity)"
+  echo "$battery"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #        DATE AND TIME
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 clockicon() {
-	echo "CLK"
+  echo "CLK"
 }
 dateinfo() {
-	echo "$(date "+%b %d %Y (%a)")"
+  echo "$(date "+%b %d %Y (%a)")"
 }
 clockinfo() {
-	echo $(date +"%H %M %S")
+  echo $(date +"%H %M %S")
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #         BAR PRINT
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-SLEEP_SEC=1
+DELAY=0.5
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-	echo -e "+@fg=1; $(cpuicon) +@fg=0; $(cpu) | \
+  bat_level=$(battery)
+  bat_stat=$(batstat)
+  if [[ "$bat_level" -le 15  && "$bat_stat" = "Discharging" ]]; then 
+      play -q -n synth .05 sine 880 vol 0.02
+      if [[ $bat_color = "+@fg=6;" ]]; then bat_color="+@fg=2;"; else bat_color="+@fg=6;"; fi;
+  elif [[ "$bat_level" -ge 85  && "$bat_stat" = "Charging" ]]; then 
+      if [[ $bat_color = "+@fg=6;" ]]; then bat_color="+@fg=1;"; else bat_color="+@fg=6;"; fi;
+  else bat_color="+@fg=6;"; fi
+  echo -e "+@fg=1; $(cpuicon) +@fg=0; $(cpu) | 
 +@fg=1; $(memicon) +@fg=0; $(mem) | \
-+@fg=2; $(pkgicon) +@fg=0; $(pkgs) | \
-+@fg=2; $(remicon) +@fg=0; $(rem) | \
++@fg=1; $(pkgicon) +@fg=0; $(pkgs) | \
++@fg=1; $(remicon) +@fg=0; $(rem) | \
 +@fg=3; $(hddicon) +@fg=0; $(hdd) | \
 +@fg=4; $(networkicon) $(connection) +@fg=0; $(ipaddress) +@fg=4; $(vpnconnection) | \
 +@fg=4; $(upicon) $(up) +@fg=0; $(downicon) +@fg=4; $(down) | \
 +@fg=3; $(entropyicon) +@fg=0; $(entropy) | \
 +@fg=5; $(volicon) +@fg=0; $(vol) | \
-+@fg=6; $(bat) | \
+$bat_color $bat_stat $bat_level % +@fg=0; | \
 +@fg=1; $(dateinfo) +@fg=4; $(clockicon) +@fg=0; $(clockinfo) |\
 "
-	sleep $SLEEP_SEC
+  sleep $DELAY
 done
+
