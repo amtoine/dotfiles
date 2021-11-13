0a2e5edad4baf89fbfb784165fe4b7da44e7ac27
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2021-11-13 10:13:32 +0100

    Personal baraction for spectrWM + pkgs.

--- scripts/spectrWM-baraction.sh
+++ scripts/spectrWM-baraction.sh
@@ -7,7 +7,7 @@
 #       /_/
 #
 # Example Bar Action Script for Linux.
-# Requires: acpi, iostat, lm-sensors, aptitude, ...
+# Requires: acpi, iostat, ...
 # Tested on: (with newest spectrwm built from source), ...
 # This config can be found on github.com/a2n-s
 
@@ -48,33 +48,17 @@ cpuicon() {
 	echo "CPU"
 }
 cpu() {
-	read cpu a b c previdle rest < /proc/stat
-	prevtotal=$((a+b+c+previdle))
-	sleep 0.5
-	read cpu a b c idle rest < /proc/stat
-	total=$((a+b+c+idle))
-	cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-	echo "$cpu%"
+	echo "$(mpstat | tail -n 1 | awk '{print $3}')%"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #            VOLUME
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 volicon() {
-	VOLONOFF="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $6}')"
-
-	VOLON="ON"
-	VOLOFF="OFF"
-
-	if [ "$VOLONOFF" = "on" ]; then
-	echo "$VOLON"
-	else
-	echo "$VOLOFF"
-	fi
+	echo "VOL"
 }
 vol() {
-	VOL="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $5}')"
-	echo "$VOL"
+	echo ""
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -84,27 +68,27 @@ pkgicon() {
 	echo "PKGs"
 }
 pkgs() {
-	pkgs="$(dpkg -l | grep -c ^i)"
+	pkgs=$(pacman -Qq | wc -l)
 	echo "$pkgs"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#          UPGRADES
+#          REMOVABLE
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-upgradeicon() {
-	echo "UP"
+remicon() {
+	echo "REM"
 }
-upgrades(){
-	upgrades="$(aptitude search '~U' | wc -l)"
-	echo "$upgrades"
+rem() {
+	rems=$(pacman -Qdt | wc -l)
+	echo "$rems"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #           NETWORK
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 networkicon() {
-	wire="$(ip a | grep "eth0\|enp" | grep inet | wc -l)"
-	wifi="$(ip a | grep wlan | grep inet | wc -l)"
+	wire="$(ip a | grep "eth" | grep inet | wc -l)"
+	wifi="$(ip a | grep "wl" | grep inet | wc -l)"
 
 	if [ $wire = 1 ]; then
 		echo "WIRE"
@@ -113,6 +97,9 @@ networkicon() {
 	else
 		echo "OFF"
 	fi
+}
+connection() {
+	echo "$(nmcli d | grep "\<connected\>" | awk '{ print substr($0, index($0,$4)) }')"
 }
 ipaddress() {
 	address="$(ip a | grep .255 | grep -v wlp | cut -d ' ' -f6 | sed 's/\/24//')"
@@ -127,39 +114,27 @@ vpnconnection() {
 		echo "NO VPN"
 	fi
 }
+upicon(){ 
+	echo "TX" 
+}
+downicon(){
+	echo "RX"
+}
+up(){
+	ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $9}'
+}
+down(){
+	ifstat | grep $(nmcli d | grep "\<connected\>" | awk '{ print $1 }') | awk '{ print $7}'
+}
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#           WEATHER
+#           ENTROPY
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-weather=$(cat ~/.config/weather.txt | sed 25q | grep value | awk '{print $2 $3}' | sed 's/\"//g')
-temp=$(cat ~/.config/weather.txt | grep temp_F | awk '{print $2}' | sed 's/\"//g' | sed 's/,//')
-
-nocloud="NO CLOUD"
-cloud="CLOUD"
-lotempicon="LOTEMP"
-midtempicon="MIDTEMP"
-hitempicon="HITEMP"
-
-#print weather info
-TEMP() {
-	echo "$temp °"
+entropyicon() {
+	echo "ENT"
 }
-tempicon() {
-	if [ "$temp" -gt 80 ]; then
-		echo $hitempicon
-	elif [ "$temp" -ge 50 ] && [ "$temp" -le 79 ]; then
-		echo $midtempicon
-	elif [ "$temp" -le 49]; then
-		echo $lotempicon
-	fi
-}
-
-weathericon() {
-	if [[ "$weather" = "Clear" ]] || [[ "$weather" = "Sunny" ]]; then
-		echo "$nocloud $weather"
-	else
-		echo "$cloud $weather"
-	fi
+entropy(){
+	echo "$(cat /proc/sys/kernel/random/entropy_avail)"
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -168,21 +143,6 @@ weathericon() {
 bat() {
 	batstat="$(cat /sys/class/power_supply/BAT0/status)"
 	battery="$(cat /sys/class/power_supply/BAT0/capacity)"
-	if [ $batstat = 'Unknown' ]; then
-		batstat=""
-	elif [[ $battery -ge 5 ]] && [[ $battery -le 19 ]]; then
-		batstat=""
-	elif [[ $battery -ge 20 ]] && [[ $battery -le 39 ]]; then
-		batstat=""
-	elif [[ $battery -ge 40 ]] && [[ $battery -le 59 ]]; then
-		batstat=""
-	elif [[ $battery -ge 60 ]] && [[ $battery -le 79 ]]; then
-		batstat=""
-	elif [[ $battery -ge 80 ]] && [[ $battery -le 95 ]]; then
-		batstat=""
-	elif [[ $battery -ge 96 ]] && [[ $battery -le 100 ]]; then
-		batstat=""
-	fi
 echo "$batstat  $battery %"
 }
 
@@ -190,31 +150,13 @@ echo "$batstat  $battery %"
 #        DATE AND TIME
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 clockicon() {
-	CLOCK=$(date '+%I')
-
-	case "$CLOCK" in
-		"00") icon="" ;;
-		"01") icon="" ;;
-		"02") icon="" ;;
-		"03") icon="" ;;
-		"04") icon="" ;;
-		"05") icon="" ;;
-		"06") icon="" ;;
-		"07") icon="" ;;
-		"08") icon="" ;;
-		"09") icon="" ;;
-		"10") icon="" ;;
-		"11") icon="" ;;
-		"12") icon="" ;;
-	esac
-
-	echo "$(date "+$icon")"
+	echo "CLK"
 }
 dateinfo() {
 	echo "$(date "+%b %d %Y (%a)")"
 }
 clockinfo() {
-	echo $(date "+%I:%M%p")
+	echo $(date +"%H %M %S")
 }
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -223,15 +165,15 @@ clockinfo() {
 SLEEP_SEC=1
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-	echo -e "\
-+@fg=1; $(cpuicon) +@fg=0; $(cpu) | \
+	echo -e "+@fg=1; $(cpuicon) +@fg=0; $(cpu) | \
 +@fg=1; $(memicon) +@fg=0; $(mem) | \
 +@fg=2; $(pkgicon) +@fg=0; $(pkgs) | \
-+@fg=2; $(upgradeicon) +@fg=0; $(upgrades) | \
++@fg=2; $(remicon) +@fg=0; $(rem) | \
 +@fg=3; $(hddicon) +@fg=0; $(hdd) | \
-+@fg=4; $(networkicon) +@fg=0; $(ipaddress) +@fg=4; $(vpnconnection) | \
++@fg=4; $(networkicon) $(connection) +@fg=0; $(ipaddress) +@fg=4; $(vpnconnection) | \
++@fg=4; $(upicon) $(up) +@fg=0; $(downicon) +@fg=4; $(down) | \
++@fg=3; $(entropyicon) +@fg=0; $(entropy) | \
 +@fg=5; $(volicon) +@fg=0; $(vol) | \
-+@fg=6; $(weathericon) $(tempicon) $(TEMP) | \
 +@fg=6; $(bat) | \
 +@fg=1; $(dateinfo) +@fg=4; $(clockicon) +@fg=0; $(clockinfo) |\
 "
