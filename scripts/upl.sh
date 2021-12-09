65437f6653de5da5ca0c787b52dba9db66eb632a
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2021-12-09 16:50:54 +0100

    upl no longer a symlink.

--- scripts/upl.sh
+++ scripts/upl.sh
@@ -1,1 +1,11 @@
-/etc/pacman.d/hooks.bin/upl.sh
+#! /bin/bash
+
+dir="$HOME/.pkgslists"
+echo "updating packages list..."
+pacman -Qqe > $dir/allpkglist.txt
+pacman -Qqent > $dir/pkglist.txt
+echo "updating dependencies list..."
+comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > $dir/optdeplist.txt
+echo "updating foreign packages list..."
+pacman -Qqem > $dir/foreignpkglist.txt
+echo "all done!"
