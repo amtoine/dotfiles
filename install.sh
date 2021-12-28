dfedf140ac8f87df56b764a125bfc46fdc0688fb
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2021-12-28 15:10:26 +0100

    Colors + useable demo preview.

--- install.sh
+++ install.sh
@@ -15,6 +15,79 @@
 # License:      https://github.com/a2n-s/dotfiles/LICENSE 
 # Contributors: Stevan Antoine
 
+# Reset
+Color_Off=$(printf '\033[0m')       # Text Reset
+
+# Regular Colors
+Blk=$(printf '\033[0;30m')          # Black
+Red=$(printf '\033[0;31m')          # Red
+Grn=$(printf '\033[0;32m')          # Green
+Ylw=$(printf '\033[0;33m')          # Yellow
+Blu=$(printf '\033[0;34m')          # Blue
+Pur=$(printf '\033[0;35m')          # Purple
+Cyn=$(printf '\033[0;36m')          # Cyan
+Wht=$(printf '\033[0;37m')          # White
+
+# Bold
+BBlack=$(printf '\033[1;30m')       # Black
+BRed=$(printf '\033[1;31m')         # Red
+BGreen=$(printf '\033[1;32m')       # Green
+BYellow=$(printf '\033[1;33m')      # Yellow
+BBlue=$(printf '\033[1;34m')        # Blue
+BPurple=$(printf '\033[1;35m')      # Purple
+BCyan=$(printf '\033[1;36m')        # Cyan
+BWhite=$(printf '\033[1;37m')       # White
+
+# Underline
+UBlack=$(printf '\033[4;30m')       # Black
+URed=$(printf '\033[4;31m')         # Red
+UGreen=$(printf '\033[4;32m')       # Green
+UYellow=$(printf '\033[4;33m')      # Yellow
+UBlue=$(printf '\033[4;34m')        # Blue
+UPurple=$(printf '\033[4;35m')      # Purple
+UCyan=$(printf '\033[4;36m')        # Cyan
+UWhite=$(printf '\033[4;37m')       # White
+
+# Background
+On_Black=$(printf '\033[40m')       # Black
+On_Red=$(printf '\033[41m')         # Red
+On_Green=$(printf '\033[42m')       # Green
+On_Yellow=$(printf '\033[43m')      # Yellow
+On_Blue=$(printf '\033[44m')        # Blue
+On_Purple=$(printf '\033[45m')      # Purple
+On_Cyan=$(printf '\033[46m')        # Cyan
+On_White=$(printf '\033[47m')       # White
+
+# High Intensity
+IBlack=$(printf '\033[0;90m')       # Black
+IRed=$(printf '\033[0;91m')         # Red
+IGreen=$(printf '\033[0;92m')       # Green
+IYellow=$(printf '\033[0;93m')      # Yellow
+IBlue=$(printf '\033[0;94m')        # Blue
+IPurple=$(printf '\033[0;95m')      # Purple
+ICyan=$(printf '\033[0;96m')        # Cyan
+IWhite=$(printf '\033[0;97m')       # White
+
+# Bold High Intensity
+BIBlack=$(printf '\033[1;90m')      # Black
+BIRed=$(printf '\033[1;91m')        # Red
+BIGreen=$(printf '\033[1;92m')      # Green
+BIYellow=$(printf '\033[1;93m')     # Yellow
+BIBlue=$(printf '\033[1;94m')       # Blue
+BIPurple=$(printf '\033[1;95m')     # Purple
+BICyan=$(printf '\033[1;96m')       # Cyan
+BIWhite=$(printf '\033[1;97m')      # White
+
+# High Intensity backgrounds
+On_IBlack=$(printf '\033[0;100m')   # Black
+On_IRed=$(printf '\033[0;101m')     # Red
+On_IGreen=$(printf '\033[0;102m')   # Green
+On_IYellow=$(printf '\033[0;103m')  # Yellow
+On_IBlue=$(printf '\033[0;104m')    # Blue
+On_IPurple=$(printf '\033[0;105m')  # Purple
+On_ICyan=$(printf '\033[0;106m')    # Cyan
+On_IWhite=$(printf '\033[0;107m')   # White
+
 files=()
 files+=(".bash_logout")
 files+=(".bash_profile")
@@ -148,9 +221,9 @@ scripts+=("ytdl.sh")
 
 DIR=`pwd`
 HDIR="${HOME}"
-CDIR=".lolconfig"
-SDIR="lolscripts"
-RDIR="lolrepos"
+CDIR=".config"
+SDIR="scripts"
+RDIR="repos"
 
 install_scripts() {
 	echo -e "\n[*] Installing scripts..."
@@ -221,27 +294,27 @@ main() {
     source: $DIR
     destination: $HDIR
 
-		[*] Choose option-
-		[1/o] Override everything.
-		[2/m] Minimal install.
-		[3/i] Interactive installation.
-
 	EOF
-  read -p "[?] What do you want the installation to be ? (1/2/3) "
-  case "$REPLY" in
-    1|"o") INST_MODE="override"
-    ;;
-    2|"m") INST_MODE="minimal"
-    ;;
-    3|"i") INST_MODE="interactive"
-    ;;
-    *) echo "defaulting..."
-       INST_MODE="interactive"
-    ;;
-  esac
-  echo "you choose the $INST_MODE mode"
-  echo "                \`--> does not have any impact for now..."
-  exit 0
+	# 	[*] Choose option-
+	# 	[1/o] Override everything.
+	# 	[2/m] Minimal install.
+	# 	[3/i] Interactive installation.
+	# EOF
+  # read -p "[?] What do you want the installation to be ? (1/2/3) "
+  # case "$REPLY" in
+  #   1|"o") INST_MODE="override"
+  #   ;;
+  #   2|"m") INST_MODE="minimal"
+  #   ;;
+  #   3|"i") INST_MODE="interactive"
+  #   ;;
+  #   *) echo "defaulting..."
+  #      INST_MODE="interactive"
+  #   ;;
+  # esac
+  # echo "you choose the $INST_MODE mode"
+  # echo "                \`--> does not have any impact for now..."
+  # exit 0
 
   prompt_for_install_and_install "scripts" "install_scripts"
   prompt_for_install_and_install "files"   "install_files"
