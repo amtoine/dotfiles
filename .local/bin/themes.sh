7fac4ce41fd776f90b7c07a0e636a423bc59159b
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-03-12 11:26:42 +0100

    scripts: themes.sh array for easier color access + better substitution logic.

--- .local/bin/themes.sh
+++ .local/bin/themes.sh
@@ -62,8 +62,8 @@ configs="qtile,dunst,alacritty,kitty,xresources,dmenu,st"
 [[ ! -v CONKY ]] && CONKY="$HOME/.config/conky"
 [[ ! -v SUCKLESS ]] && SUCKLESS="$HOME/ghq/git.suckless.org"
 [[ ! -v XRESOURCES ]] && XRESOURCES="$HOME/.Xresources"
-_nb_colors=20
 _columns=(name bg fg sel_bg sel_fg 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+_nb_colors="$(( ${#_columns[@]}-1 ))"
 
 update () {
   #
@@ -184,10 +184,13 @@ qtile_cfg () {
   #
 
   # change all the fields
+  echo -e "Changing ${Dst}qtile${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
   fields=(bg fg sel_bg sel_fg color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15)
   for i in $(seq 0 19);
   do
-    sed -i "s/\(\s*\"${fields[$i]}\": \"\)#......\",\s*\$/\1$(echo "$1" | awk -F, "{print \$$(($i+1))}")\",/" "$QTILE/theme.py"
+    sed -i "s/\(\s*\"${fields[$i]}\": \"\)#......\",\s*\$/\1${colors[$i]}\",/" "$QTILE/theme.py"
   done
   # change the name of the current theme
   sed -i "s/# current: .*/# current: $2/" "$QTILE/theme.py"
@@ -197,10 +200,10 @@ qtile_cfg () {
     qtile cmd-obj -o cmd -f restart
   fi
   # changes the conky as well
-  sed -i "s/\(\s\+color0\s\+=\s\+\)'#......',/\1'$(echo "$1" | awk -F, "{print \$9}")',/" "$CONKY/qtile.conkyrc"
-  sed -i "s/\(\s\+color1\s\+=\s\+\)'#......',/\1'$(echo "$1" | awk -F, "{print \$12}")',/" "$CONKY/qtile.conkyrc"
-  sed -i "s/\(\s\+color2\s\+=\s\+\)'#......',/\1'$(echo "$1" | awk -F, "{print \$11}")',/" "$CONKY/qtile.conkyrc"
-  sed -i "s/\(\s\+color3\s\+=\s\+\)'#......',/\1'$(echo "$1" | awk -F, "{print \$7}")',/" "$CONKY/qtile.conkyrc"
+  sed -i "s/\(\s\+color0\s\+=\s\+\)'#......',/\1'${colors[8]}',/" "$CONKY/qtile.conkyrc"
+  sed -i "s/\(\s\+color1\s\+=\s\+\)'#......',/\1'${colors[11]}',/" "$CONKY/qtile.conkyrc"
+  sed -i "s/\(\s\+color2\s\+=\s\+\)'#......',/\1'${colors[10]}',/" "$CONKY/qtile.conkyrc"
+  sed -i "s/\(\s\+color3\s\+=\s\+\)'#......',/\1'${colors[6]}',/" "$CONKY/qtile.conkyrc"
   sed -i "s/\(\s\+--Colors \).*/\1$2/" "$CONKY/qtile.conkyrc"
 }
 
@@ -209,53 +212,40 @@ dunst_cfg () {
   # change the theme of the dunst notification server.
   #
   # update the theme inside the dunstrc config file
-  bg=$(echo "$1" | awk -F, "{print \$1}")
-  cfg=$(echo "$1" | awk -F, "{print \$6}")
-  cfc=$(echo "$1" | awk -F, "{print \$16}")
-  nfg=$(echo "$1" | awk -F, "{print \$9}")
-  lfg=$(echo "$1" | awk -F, "{print \$12}")
-  sed -i "s/\(\s\+background\s\+=\s\+\"\)#......\(\"  # URGENCY_CRITICAL BG\)/\1$bg\2/" "$DUNST"
-  sed -i "s/\(\s\+background\s\+=\s\+\"\)#......\(\"  # URGENCY_NORMAL BG\)/\1$bg\2/" "$DUNST"
-  sed -i "s/\(\s\+background\s\+=\s\+\"\)#......\(\"  # URGENCY_LOW BG\)/\1$bg\2/" "$DUNST"
-  sed -i "s/\(\s\+foreground\s\+=\s\+\"\)#......\(\"  # URGENCY_CRITICAL FG\)/\1$cfg\2/" "$DUNST"
-  sed -i "s/\(\s\+foreground\s\+=\s\+\"\)#......\(\"  # URGENCY_NORMAL FG\)/\1$nfg\2/" "$DUNST"
-  sed -i "s/\(\s\+foreground\s\+=\s\+\"\)#......\(\"  # URGENCY_LOW FG\)/\1$lfg\2/" "$DUNST"
-  sed -i "s/\(\s\+frame_color\s\+=\s\+\"\)#......\(\"  # URGENCY_CRITICAL FC\)/\1$cfc\2/" "$DUNST"
+  echo -e "Changing ${Dst}dunst${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
+  a=("background" "background" "background" "foreground" "foreground" "foreground" "frame_color")
+  b=("CRITICAL"   "NORMAL"     "LOW"        "CRITICAL"   "NORMAL"     "LOW"        "CRITICAL")
+  c=(0            0            0            5            8            11           15)
+  for ((i = 0; i < 7; i++));
+  do
+    sed -i "s/\(\s\+${a[$i]}\s\+=\s\+\"\)#......\(\"  # ${b[$i]}\)/\1${colors[${c[$i]}]}\2/" "$DUNST"
+  done
   # change the name of the theme.
   sed -i "s/# THEME: .*/# THEME: $2/" "$DUNST"
   # restart dunst and show a preview
   killall dunst
   dunst -conf "$DUNST" &
-  dunstify "$(echo $theme | sed 's/.conf$//')" "this is a critical test" -u critical -t 10000
-  dunstify "$(echo $theme | sed 's/.conf$//')" "this is a normal test" -u normal -t 10000
-  dunstify "$(echo $theme | sed 's/.conf$//')" "this is a low test" -u low -t 10000
-  dunstify "$(echo $theme | sed 's/.conf$//') (sample progress bar)" -h "int:value:40" -u low --icon=/usr/share/icons/Adwaita/16x16/legacy/audio-volume-medium.png -t 10000
+  dunstify "${theme//\.conf$//}" "this is a critical test" -u critical -t 10000
+  dunstify "${theme//\.conf$//}" "this is a normal test" -u normal -t 10000
+  dunstify "${theme//\.conf$//}" "this is a low test" -u low -t 10000
+  dunstify "${theme//\.conf$//} (sample progress bar)" -h "int:value:40" -u low --icon=/usr/share/icons/Adwaita/16x16/legacy/audio-volume-medium.png -t 10000
 }
 
 alacritty_cfg () {
   #
   # change the theme of the alacritty terminal emulator.
   #
-  sed -i "s/\(\s*background\s*:\s*'0x\)......\('\s*# PRIMARY\)/\1$(echo "$1" | awk -F, '{print $1}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*foreground\s*:\s*'0x\)......\('\s*# PRIMARY\)/\1$(echo "$1" | awk -F, '{print $2}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*text\s*:\s*'0x\)......\('\s*# CURSOR\)/\1$(echo "$1" | awk -F, '{print $3}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*cursor\s*:\s*'0x\)......\('\s*# CURSOR\)/\1$(echo "$1" | awk -F, '{print $4}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*black\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $5}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*red\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $6}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*green\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $7}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*yellow\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $8}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*blue\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $9}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*magenta\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $10}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*cyan\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $11}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*white\s*:\s*'0x\)......\('\s*# NORMAL\)/\1$(echo "$1" | awk -F, '{print $12}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*black\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $13}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*red\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $14}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*green\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $15}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*yellow\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $16}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*blue\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $17}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*magenta\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $18}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*cyan\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $19}' | sed 's/#//')\2/" "$ALACRITTY"
-  sed -i "s/\(\s*white\s*:\s*'0x\)......\('\s*# BRIGHT\)/\1$(echo "$1" | awk -F, '{print $20}' | sed 's/#//')\2/" "$ALACRITTY"
+  echo -e "Changing ${Dst}alacritty${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
+  a=(background foreground text   cursor black  red    green  yellow blue   magenta cyan   white  black  red    green  yellow blue   magenta cyan   white)
+  b=(PRIMARY    PRIMARY    CURSOR CURSOR NORMAL NORMAL NORMAL NORMAL NORMAL NORMAL  NORMAL NORMAL BRIGHT BRIGHT BRIGHT BRIGHT BRIGHT BRIGHT  BRIGHT BRIGHT)
+  for ((i = 0; i < 20; i++));
+  do
+    sed -i "s/\(\s*${a[$i]}\s*:\s*'0x\)......\('\s*# ${b[$i]}\)/\1${colors[$i]//#/}\2/" "$ALACRITTY"
+  done
   # change the name of the theme.
   sed -i "s/\(\s*# COLORSCHEME: \).*/\1$2/" "$ALACRITTY"
 }
@@ -264,26 +254,14 @@ kitty_cfg () {
   #
   # change the theme of the kitty terminal emulator.
   #
-  sed -i "s/\(^background\s*\)#....../\1$(echo "$1" | awk -F, '{print $1}')/" "$KITTY"
-  sed -i "s/\(^foreground\s*\)#....../\1$(echo "$1" | awk -F, '{print $2}')/" "$KITTY"
-  sed -i "s/\(^selection_background \s*\)#....../\1$(echo "$1" | awk -F, '{print $3}')/" "$KITTY"
-  sed -i "s/\(^selection_foreground \s*\)#....../\1$(echo "$1" | awk -F, '{print $4}')/" "$KITTY"
-  sed -i "s/\(^color0\s*\)#....../\1$(echo "$1" | awk -F, '{print $5}')/" "$KITTY"
-  sed -i "s/\(^color1\s*\)#....../\1$(echo "$1" | awk -F, '{print $6}')/" "$KITTY"
-  sed -i "s/\(^color2\s*\)#....../\1$(echo "$1" | awk -F, '{print $7}')/" "$KITTY"
-  sed -i "s/\(^color3\s*\)#....../\1$(echo "$1" | awk -F, '{print $8}')/" "$KITTY"
-  sed -i "s/\(^color4\s*\)#....../\1$(echo "$1" | awk -F, '{print $9}')/" "$KITTY"
-  sed -i "s/\(^color5\s*\)#....../\1$(echo "$1" | awk -F, '{print $10}')/" "$KITTY"
-  sed -i "s/\(^color6\s*\)#....../\1$(echo "$1" | awk -F, '{print $11}')/" "$KITTY"
-  sed -i "s/\(^color7\s*\)#....../\1$(echo "$1" | awk -F, '{print $12}')/" "$KITTY"
-  sed -i "s/\(^color8\s*\)#....../\1$(echo "$1" | awk -F, '{print $13}')/" "$KITTY"
-  sed -i "s/\(^color9\s*\)#....../\1$(echo "$1" | awk -F, '{print $14}')/" "$KITTY"
-  sed -i "s/\(^color10\s*\)#....../\1$(echo "$1" | awk -F, '{print $15}')/" "$KITTY"
-  sed -i "s/\(^color11\s*\)#....../\1$(echo "$1" | awk -F, '{print $16}')/" "$KITTY"
-  sed -i "s/\(^color12\s*\)#....../\1$(echo "$1" | awk -F, '{print $17}')/" "$KITTY"
-  sed -i "s/\(^color13\s*\)#....../\1$(echo "$1" | awk -F, '{print $18}')/" "$KITTY"
-  sed -i "s/\(^color14\s*\)#....../\1$(echo "$1" | awk -F, '{print $19}')/" "$KITTY"
-  sed -i "s/\(^color15\s*\)#....../\1$(echo "$1" | awk -F, '{print $20}')/" "$KITTY"
+  echo -e "Changing ${Dst}kitty${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
+  a=(background foreground selection_background selection_foreground color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15)
+  for ((i = 0; i < 20; i++));
+  do
+    sed -i "s/\(^${a[$i]}\s*\)#....../\1${colors[$i]}/" "$KITTY"
+  done
   # change the name of the theme.
   sed -i "s/\(^# THEME: \).*/\1$2/" "$KITTY"
 }
@@ -292,15 +270,17 @@ dmenu_cfg () {
   #
   # change the theme of dmenu.
   #
+  echo -e "Changing ${Dst}dmenu${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
   [ ! -f "$SUCKLESS/dmenu/config.h" ] && cp "$SUCKLESS/dmenu/config.def.h" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeSel\]            = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $5}')\", \"$(echo "$1" | awk -F, '{print $10}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeSelOut\]         = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $5}')\", \"$(echo "$1" | awk -F, '{print $11}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeMid\]            = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $2}')\", \"$(echo "$1" | awk -F, '{print $3}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeMidOut\]         = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $2}')\", \"$(echo "$1" | awk -F, '{print $13}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeSelHighlight\]   = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $7}')\", \"$(echo "$1" | awk -F, '{print $5}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeNormHighlight\]  = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $7}')\", \"$(echo "$1" | awk -F, '{print $5}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeNorm\]           = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $2}')\", \"$(echo "$1" | awk -F, '{print $1}')\2/" "$SUCKLESS/dmenu/config.h"
-  sed -i "s/\(\[SchemeOut\]            = { \"\)#......\", \"#......\(\" },\)/\1$(echo "$1" | awk -F, '{print $5}')\", \"$(echo "$1" | awk -F, '{print $2}')\2/" "$SUCKLESS/dmenu/config.h"
+  a=(SchemeSel SchemeSelOut SchemeMid SchemeMidOut SchemeSelHighlight SchemeNormHighlight SchemeNorm SchemeOut)
+  b=(4         4            1         1            6                  6                   1          4)
+  c=(9         10           2         12           4                  4                   0          1)
+  for ((i = 0; i < 8; i++));
+  do
+    sed -i "s/\(\[${a[$i]}\]\s\+= { \"\)#......\", \"#......\(\" },\)/\1${colors[${b[$i]}]}\", \"${colors[${c[$i]}]}\2/" "$SUCKLESS/dmenu/config.h"
+  done
   # change the name of the theme.
   sed -i "s/\(^\/\/ THEME: \).*/\1$2/" "$SUCKLESS/dmenu/config.h"
   cd "$SUCKLESS/dmenu" > /dev/null || exit 1
@@ -312,26 +292,15 @@ st_cfg () {
   #
   # change the theme of st.
   #
+  echo -e "Changing ${Dst}st${Off}'s theme to ${Src}'$2'${Off}"
+  colors="$1"
+
   [ ! -f "$SUCKLESS/st/config.h" ] && cp "$SUCKLESS/st/config.def.h" "$SUCKLESS/st/config.h"
-  sed -i "s/\[16\] = \"#......\",/[16] = \"$(echo "$1" | awk -F, '{print $1}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[17\] = \"#......\",/[17] = \"$(echo "$1" | awk -F, '{print $2}')\",/" "$SUCKLESS/st/config.h"
-  # no selection foreground nor background
-  sed -i "s/\[0\] = \"#......\",/[0] = \"$(echo "$1" | awk -F, '{print $5}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[1\] = \"#......\",/[1] = \"$(echo "$1" | awk -F, '{print $6}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[2\] = \"#......\",/[2] = \"$(echo "$1" | awk -F, '{print $7}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[3\] = \"#......\",/[3] = \"$(echo "$1" | awk -F, '{print $8}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[4\] = \"#......\",/[4] = \"$(echo "$1" | awk -F, '{print $9}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[5\] = \"#......\",/[5] = \"$(echo "$1" | awk -F, '{print $10}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[6\] = \"#......\",/[6] = \"$(echo "$1" | awk -F, '{print $11}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[7\] = \"#......\",/[7] = \"$(echo "$1" | awk -F, '{print $12}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[8\] = \"#......\",/[8] = \"$(echo "$1" | awk -F, '{print $13}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[9\] = \"#......\",/[9] = \"$(echo "$1" | awk -F, '{print $14}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[10\] = \"#......\",/[10] = \"$(echo "$1" | awk -F, '{print $15}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[11\] = \"#......\",/[11] = \"$(echo "$1" | awk -F, '{print $16}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[12\] = \"#......\",/[12] = \"$(echo "$1" | awk -F, '{print $17}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[13\] = \"#......\",/[13] = \"$(echo "$1" | awk -F, '{print $18}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[14\] = \"#......\",/[14] = \"$(echo "$1" | awk -F, '{print $19}')\",/" "$SUCKLESS/st/config.h"
-  sed -i "s/\[15\] = \"#......\",/[15] = \"$(echo "$1" | awk -F, '{print $20}')\",/" "$SUCKLESS/st/config.h"
+  a=(4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 0 1)
+  for ((i = 0; i < 18; i++));
+  do
+    sed -i "s/\[$i\] = \"#......\",/[$i] = \"${colors[${a[$i]}]}\",/" "$SUCKLESS/st/config.h"
+  done
   # change the name of the theme.
   sed -i "s/\(^\/\/ THEME: \).*/\1$2/" "$SUCKLESS/st/config.h"
   cd "$SUCKLESS/st" > /dev/null || exit 1
@@ -344,28 +313,15 @@ xresources_cfg () {
   #
   # change the theme of doom emacs and neovim.
   #
-  sed -i "s/^\(\*fadeColor:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $5}')/" "$XRESOURCES"
-  sed -i "s/^\(\*background:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $1}')/" "$XRESOURCES"
-  sed -i "s/^\(\*foreground:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $2}')/" "$XRESOURCES"
-  sed -i "s/^\(\*pointerColorBackground:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $3}')/" "$XRESOURCES"
-  sed -i "s/^\(\*pointerColorForeground:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $4}')/" "$XRESOURCES"
-  sed -i "s/^\(\*cursorColor:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $4}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color0:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $5}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color1:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $6}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color2:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $7}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color3:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $8}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color4:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $9}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color5:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $10}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color6:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $11}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color7:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $12}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color9:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $13}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color8:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $14}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color10:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $15}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color11:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $16}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color12:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $17}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color13:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $18}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color14:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $19}')/" "$XRESOURCES"
-  sed -i "s/^\(\*color15:\s\+\)#....../\1$(echo "$1" | awk -F, '{print $20}')/" "$XRESOURCES"
+  echo -e "Changing ${Dst}xresources${Off}' theme to ${Src}'$2'${Off}"
+  colors="$1"
+
+  a=(background foreground pointerColorBackground pointerColorForeground cursorColor fadeColor color0 color1 color2 color3 color4 color5 color6 color7 color9 color8 color10 color11 color12 color13 color14 color15)
+  b=(0          1          2                      3                      3           4         4      5      6      7      8      9      10     11     12     13     14      15      16      17      18      19)
+  for ((i = 0; i < 22; i++));
+  do
+    sed -i "s/^\(\*${a[$i]}:\s\+\)#....../\1${colors[${b[$i]}]}/" "$XRESOURCES"
+  done
   # change the name of the theme.
   sed -i "s/\(^! THEME: \).*/\1$2/" "$XRESOURCES"
   xrdb "$XRESOURCES"
@@ -380,17 +336,22 @@ theme () {
   theme=$(echo "$1" | sed 's/^\s*//g; s/\s*$//g')
   [ ! "$theme" ] && theme="DEFAULT_TO_DMENU"
   if [[ -f "$colordatabase" ]]; then
-    if ! awk -F, '{print $1}' "$colordatabase" | grep -we "$(echo $theme | sed 's/ /_/g')" -q;
+    # extract the theme colors, either directly if -t is valid
+    # or with dmenu
+    if ! awk -F, '{print $1}' "$colordatabase" | grep -we "${theme// /_}" -q;
     then
       echo -e "${Err}'$theme' not in $colordatabase${Off}"
       theme=$(tail -n +2 "$colordatabase" | awk -F, '{print $1}' | sed 's/_/ /g' | dmenu -bw 5 -c -l 20 -i -p "Choose a theme: ")
       [ ! "$theme" ] && { echo -e "${Wrn}No theme selected${Off}"; exit 0; }
       echo -e "${Ok}'$theme' selected${Off}"
     else 
-      echo -e "${Tip}'$theme' found in $colordatabase${Off}"
+      echo -e "'$theme' ${Tip}found${Off} in $colordatabase"
     fi
-    theme=$(echo "$theme" | sed 's/ /_/g')
-    colors=$(grep -w "$theme" "$colordatabase" | sed "s/$theme,//")
+    theme="${theme// /_}"
+    # put the colors in an array for easier color access later
+    colors=($(grep -w "$theme" "$colordatabase" | sed "s/$theme,//; s/,/ /g"))
+
+    # modify the configs accordingly
     for config in $(echo "$2" | tr ',' ' ');do
       case "$config" in
         qtile ) qtile_cfg "$colors" "$theme";;
