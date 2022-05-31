50e0c8b1ea3815f3e52ffb010ee6738c2023aeb5
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-05-31 11:05:06 +0200

    (bspwm) refactor the autostart script with functions

--- .config/bspwm/scripts/autostart.sh
+++ .config/bspwm/scripts/autostart.sh
@@ -31,12 +31,33 @@ error () {
   fi
 }
 
+does_command_exist () {
+  # Check if a command exist on the system.
+  #
+  # Args:
+  #   $1: the name of the command.
+  #
+  # Returns:
+  #   code: the same error code, saying whether the command exists or not.
+  command -v "$1" &> /dev/null
+}
+
+kill_if_running () {
+  # Kill a program only if running.
+  #
+  # Args:
+  #   $1: the program to kill.
+  #
+  if pgrep -f "$1" 1> /dev/null; then
+    killall "$1"
+  fi
+}
 
 NO_BEGINNER="$HOME/.local/share/bspwm/nobeginner"
 run_conky () {
   # Run all the conky needed by bspwm, only once.
   #
-  # Now, there is a help conky at $CONKY_HELP and a clock at $CONKY_CLOCK
+  # Now, there is a help conky at $CONKY_HELP and a clock at $CONKY_CLOCK.
   #
   if [ ! -f "$NO_BEGINNER" ]; then
     conky --config "$WM_CONKY_HELP" --daemonize
@@ -50,51 +71,54 @@ run_conky () {
 bash "$HOME/.config/bspwm/scripts/sxhkd_start.sh"
 
 # start the `dunst` notification server in the background
-if command -v dunst &> /dev/null; then
-  killall dunst
+if does_command_exist dunst; then
+  kill_if_running dunst
   dunst -conf ~/.config/dunst/dunstrc &
   error "$?" "dunst started successfully" "dunst failed to start"
 fi
 
-if command -v polybar &> /dev/null; then
-  [ -n "$WM_USE_POLYBAR" ] && {
-    bash "$WM_POLYBAR" --"$WM_POLYBAR_THEME" &
-    error "$?" "polybar started successfully" "polybar failed to start"
-  }
+if does_command_exist polybar &&  -n "$WM_USE_POLYBAR" ]; then
+  bash "$WM_POLYBAR" --"$WM_POLYBAR_THEME" &
+  error "$?" "polybar started successfully" "polybar failed to start"
 fi
 
 # open the help only when first time
-if command -v conky &> /dev/null; then
-  killall conky
+if does_command_exist conky; then
+  kill_if_running conky
   run_conky
 fi
 
-if command -v nm-applet &> /dev/null; then
-  killall nm-applt
-  nm-applt &
-  error "$?" "nm-applet started successfully" "nm-applet failed to start"
-fi
-if command -v volumeicon &> /dev/null; then
-  killall volumeicon
-  volumeicon &
-  error "$?" "volumeicon started successfully" "volumeicon failed to start"
+if [ -n "$WM_USE_SYSTRAY" ]; then
+  if does_command_exist nm-applet; then
+    kill_if_running nm-applt
+    nm-applt &
+    error "$?" "nm-applet started successfully" "nm-applet failed to start"
+  fi
+  if does_command_exist volumeicon; then
+    kill_if_running volumeicon
+    volumeicon &
+    error "$?" "volumeicon started successfully" "volumeicon failed to start"
+  fi
+else
+    kill_if_running nm-applt
+    kill_if_running volumeicon
 fi
 
 # start the compositor
-if command -v picom &> /dev/null; then
-  killall picom
-  error "$?" "picom successfully killed" "picom was not killed"
+if does_command_exist picom; then
+  kill_if_running picom
   picom --experimental-backends --daemon --animations
   error "$?" "picom started successfully" "picom failed to start"
 fi
 
 # choose a random wallpaper
-if command -v feh &> /dev/null; then
+if does_command_exist feh; then
   feh --randomize /usr/share/backgrounds/* --bg-fill
+  error "$?" "wallpaper(s) set" "wallpaper(s) not set"
 fi
 
 # start the autolock
-if command -v xautolock &> /dev/null; then
+if does_command_exist xautolock; then
   xautolock -exit
   xautolock -time 15 -locker "$LOCKER" &
   error "$?" "xautolock started successfully" "xautolock failed to start"
@@ -107,7 +131,7 @@ xset s 0
 xset -dpms
 
 # start the `emacs` server in the background
-if command -v emacs &> /dev/null; then
+if does_command_exist emacs; then
   if ! pgrep -f "emacs --daemon" 1> /dev/null; then
     emacs --daemon &
     error "$?" "emacs server started successfully" "emacs server failed to start"
