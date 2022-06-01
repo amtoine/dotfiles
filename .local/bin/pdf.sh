9c1b425d199668c18d4ed6e1403589a87dd82c38
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-01 17:14:05 +0200

    (pdf.sh) add a notification to tell it's working

--- .local/bin/pdf.sh
+++ .local/bin/pdf.sh
@@ -2,6 +2,9 @@
 #
 # Shows a list of pdf in the $HOME directory of the user inside dmenu and open the selected ones with okular.
 
-choices=$(find "$HOME" -type f -name "*\.pdf" -not -path "$HOME/.cache/*" | dmenu -c -l 10 -bw 5 -p "Choose a pdf:" | sort -u)
+BASE="$HOME"
+EXCLUDE="$HOME/.cache"
+dunstify "pdf.sh" "pulling pdfs from:\n'$BASE'\nexcluding:\n'$EXCLUDE'"
+choices=$(find "$BASE" -type f -name "*\.pdf" -not -path "$EXCLUDE/*" | dmenu -c -l 10 -bw 5 -p "Choose a pdf:" | sort -u)
 [ ! "$choices" ] && { echo -e "User choose to exit"; exit 1; }
 okular $choices
