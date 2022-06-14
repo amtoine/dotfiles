d2ffea27249905f58410ed6ebf403498d55737ee
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-14 18:39:28 +0200

    (pkgs) change the name of my custom packages
    Addresses the changes made in
    https://github.com/a2n-s/pkgbuilds/issues/2

--- .moc/on_song_change.sh
+++ .moc/on_song_change.sh
@@ -5,6 +5,6 @@
 #   $2: the title.
 #   $3: the duration.
 
-[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons"
+[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons-git"
 
 dunstify --urgency normal "$1" "$2\n$3" --icon "$ICONS/audio-music.png"
--- .moc/on_stop.sh
+++ .moc/on_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons"
+[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons-git"
 
 dunstify --urgency normal "Music On Console" "Server stopped" --icon "$ICONS/audio-music.png"
