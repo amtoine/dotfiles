c2e384ef0e04611fd62d80e62d2025aceb20805f
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-19 13:00:14 +0200

    (emacs) fix typo giving "unknown user-mail-address" in mu4e

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Antoine Stevan"
-      user-mail-address "stevan.antoin@gmail.com")
+      user-mail-address "stevan.antoine@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
