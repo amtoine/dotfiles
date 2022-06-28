5746466f652ce483468d238519219319eb0b0176
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-28 18:45:19 +0200

    (moc) fix paths to the icons

--- .moc/on_stop.sh
+++ .moc/on_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons-git"
+[[ ! -v ICONS ]] && ICONS="/usr/share/icons/a2n-s-icons-git/dark/128x128"
 
-dunstify --urgency normal "Music On Console" "Server stopped" --icon "$ICONS/audio-music.png"
+dunstify --urgency normal "Music On Console" "Server stopped" --icon "$ICONS/audio/stop.png"
