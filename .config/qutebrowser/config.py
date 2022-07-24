b06d131c7e238b5327b2f14370f7bbec40057307
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-07-24 10:23:14 +0200

    (qute) add ',' to custom bindings to avoid conflicts

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2312,7 +2312,7 @@ c.url.start_pages = ["file:///home/ants/.config/www/html/homepage.html"]
 # config.bind('{{', 'navigate prev -t')
 # config.bind('}}', 'navigate next -t')
 
-config.bind("cs", "config-source")
+config.bind(",cs", "config-source")
 
 command = "set content.user_stylesheets"
 css_path = "~/ghq/github.com/alphapapa/solarized-everything-css/css"
@@ -2328,14 +2328,14 @@ for binding, theme in theme_bindings.items():
     theme_path = f'{css_path}/{theme}/{theme}-all-sites.css' if theme else "\"\""
     config.bind(binding, f'{command} {theme_path}')
 
-config.bind('M', 'hint links spawn mpv --ytdl-format="bestvideo[height<=480]+bestaudio[ext=m4a]" {hint-url}')
-config.bind('P', 'hint links spawn mpv --ytdl-format="bestaudio[ext=m4a]" {hint-url} --no-video')
-config.bind('F', 'hint links spawn firefox {hint-url}')
-config.bind('Z', 'hint links spawn st -c qutebrowser-ytdl-download -e youtube-dl {hint-url}')
+config.bind(',M', 'hint links spawn mpv --ytdl-format="bestvideo[height<=480]+bestaudio[ext=m4a]" {hint-url}')
+config.bind(',P', 'hint links spawn mpv --ytdl-format="bestaudio[ext=m4a]" {hint-url} --no-video')
+config.bind(',F', 'hint links spawn firefox {hint-url}')
+config.bind(',Z', 'hint links spawn st -c qutebrowser-ytdl-download -e youtube-dl {hint-url}')
 
-config.bind('xb', 'config-cycle statusbar.show always never')
-config.bind('xt', 'config-cycle tabs.show always never')
-config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
+config.bind(',xb', 'config-cycle statusbar.show always never')
+config.bind(',xt', 'config-cycle tabs.show always never')
+config.bind(',xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
 
 ## Bindings for caret mode
 # config.bind('$', 'move-to-end-of-line', mode='caret')
