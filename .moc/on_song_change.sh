7eecade498dafecaa6e7c7d3b532afa6e1a97cb1
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-08-24 19:06:10 +0200

    (moc) use stickers icons from amtoine-icons-git

--- .moc/on_song_change.sh
+++ .moc/on_song_change.sh
@@ -5,6 +5,6 @@
 #   $2: the title.
 #   $3: the duration.
 
-[[ ! -v ICONS ]] && ICONS="/usr/share/icons/amtoine-icons-git/dark/128x128"
+[[ ! -v ICONS ]] && ICONS="/usr/share/icons/amtoine-icons-git/stickers/100x100"
 
 dunstify --urgency normal "$1" "$2\n$3" --icon "$ICONS/audio/music.png"
--- .moc/on_stop.sh
+++ .moc/on_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[[ ! -v ICONS ]] && ICONS="/usr/share/icons/amtoine-icons-git/dark/128x128"
+[[ ! -v ICONS ]] && ICONS="/usr/share/icons/amtoine-icons-git/stickers/100x100"
 
 dunstify --urgency normal "Music On Console" "Server stopped" --icon "$ICONS/audio/stop.png"
