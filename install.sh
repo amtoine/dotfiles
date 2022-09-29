0d5021094126376f06fe5d0701bb55c433d4b41d
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-09-29 11:31:21 +0200

    Add first test of the installation script
    Not tested yet

---
+++ install.sh
@@ -0,0 +1,35 @@
+# Update
+sudo pacman -S archlinux-keyring
+sudo pacman -Syu
+
+# Install tools for building dependencies
+sudo pacman -S git nushell
+
+# Clone dotfiles
+git clone --bare https://github.com/atxr/dotfiles-1 .dotfiles
+git clone https://github.com/amtoine/pkgbuilds
+
+# Build pkgbuilds
+cd pkgbuilds
+./install.sh paru x86_64/amtoine-scripts-git/ x86_64/amtoine-sounds-git/ x86_64/amtoine-wallpapers-git/ x86_64/amtoine-applications-git/ x86_64/amtoine-icons-git/
+cd
+sudo rm -r pkgbuilds
+
+# Install dependencies
+nu -c 'paru -S (open pkgs.toml | get pkgs.pacman.native.package)'
+
+# Install dmenu-flexipatch
+git clone https://github.com/bakkeby/dmenu-flexipatch dmenu
+cp .config/dmenu-flexipatch/patches.h dmenu
+cd dmenu 
+make clean install
+cd 
+sudo rm -r dmenu
+
+# Activate useful system
+sudo systemctl enable sddm
+sudo systemctl enable NetworkManager
+
+# Greet
+echo 'Installation completed! Please reboot your computer.'
+
