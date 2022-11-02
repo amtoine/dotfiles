amtoine/dotfiles

commit 756aa0344a387d0148a4cd8714a5abd8f108bd52

Fix nushell issue with $HOME

install.sh
@@ -32,7 +32,7 @@ git clone https://github.com/goatfiles/pkgbuilds /tmp/pkgbuilds
 )
 
 # Install dependencies
-nu -c "paru -S (open $HOME/pkgs.toml | get pkgs.pacman.explicit.package | find --invert --regex 'amtoine|wallpapers')"
+nu -c 'open $"($env.HOME)/pkgs.toml" | get pkgs.pacman.explicit.package | find --invert --regex "amtoine|wallpapers"'
 
 # Install dmenu-flexipatch
 (

commit 497f5f1683824d37f9cfb4fb440a203c84d60b42

Fix bug introduced in 756aa03
Add missing `| paru -S`

install.sh
@@ -32,7 +32,7 @@ git clone https://github.com/goatfiles/pkgbuilds /tmp/pkgbuilds
 )
 
 # Install dependencies
-nu -c 'open $"($env.HOME)/pkgs.toml" | get pkgs.pacman.explicit.package | find --invert --regex "amtoine|wallpapers"'
+nu -c 'open $"($env.HOME)/pkgs.toml" | get pkgs.pacman.explicit.package | find --invert --regex "amtoine|wallpapers" | paru -S'
 
 # Install dmenu-flexipatch
 git clone https://github.com/bakkeby/dmenu-flexipatch /tmp/dmenu