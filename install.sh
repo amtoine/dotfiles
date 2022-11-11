0c24b93bad8217e5a6fde3cb22535ca83c59e0f2
Alexandre Tullot <alexandre.tullot@protonmail.com>
2022-11-11 16:58:36 +0100

    Add installation script (#13)
    Co-authored-by: amtoine <44101798+AntoineStevan@users.noreply.github.com>
    Co-authored-by: atxr <alexamdre.protonmail.com>

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+# Update
+sudo pacman -S archlinux-keyring
+sudo pacman -Syu
+
+# Install tools for building dependencies
+sudo pacman -S git nushell cargo feh
+
+# Clone dotfiles
+git clone --bare https://github.com/goatfiles/dotfiles $HOME/.dotfiles
+cfg="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+$cfg reset --hard
+$cfg config --local status.showUntrackedFiles no
+$cfg config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+# Clean and clone pkgbuilds
+if [[ -d /tmp/pkgbuilds ]]; then sudo rm -r /tmp/pkgbuilds; fi
+git clone https://github.com/goatfiles/pkgbuilds /tmp/pkgbuilds
+
+# Build pkgbuilds
+(
+    cd /tmp/pkgbuilds
+    ./install.sh x86_64/paru
+    ./install.sh x86_64/amtoine-scripts-git/
+    ./install.sh x86_64/amtoine-sounds-git/
+    ./install.sh x86_64/amtoine-wallpapers-git/
+    ./install.sh x86_64/amtoine-applications-git/
+    ./install.sh x86_64/amtoine-icons-git/
+    ./install.sh x86_64/junnunkarim-wallpapers-git
+    ./install.sh x86_64/mut-ex-wallpapers-git
+)
+
+# Install dependencies
+nu -c 'paru -S (open $"($env.HOME)/pkgs.toml" | get pkgs.pacman.explicit.package | find --invert --regex "amtoine|wallpapers")'
+
+# Install dmenu-flexipatch
+git clone https://github.com/bakkeby/dmenu-flexipatch /tmp/dmenu
+cp $HOME/.config/dmenu-flexipatch/patches.h /tmp/dmenu
+(
+    cd /tmp/dmenu
+    sudo make clean install
+)
+
+# Activate useful system
+sudo systemctl enable sddm
+sudo systemctl enable NetworkManager
+
+# Greet
+echo 'Installation completed! Please reboot your computer.'
+
