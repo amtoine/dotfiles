84428337c68fe2536bc0a69d4fdee9e489725bf7
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2023-01-23 19:51:23 +0100

    FEATURE: some work on the install script (#68)
    This PR should address most of the points in #35.

--- install.sh
+++ install.sh
@@ -10,52 +10,182 @@
 #*              ATXR:    https://github.com/atxr    atxr#6214    3B25AF716B608D41AB86C3D20E55E4B1DE5B2C8B
 #*
 
-# Update
-sudo pacman -S archlinux-keyring
-sudo pacman -Syu
 
-# Install tools for building dependencies
-sudo pacman -S git nushell cargo feh
-
-# Clone dotfiles
-git clone --bare https://github.com/goatfiles/dotfiles $HOME/.dotfiles
-cfg="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
-$cfg reset --hard
-$cfg config --local status.showUntrackedFiles no
-$cfg config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-
-# Clean and clone pkgbuilds
-if [[ -d /tmp/pkgbuilds ]]; then sudo rm -r /tmp/pkgbuilds; fi
-git clone https://github.com/goatfiles/pkgbuilds /tmp/pkgbuilds
-
-# Build pkgbuilds
-(
-    cd /tmp/pkgbuilds
-    ./install.sh x86_64/goat-paru-git/
-    ./install.sh x86_64/goat-scripts-git/
-    ./install.sh x86_64/goat-sounds-git/
-    ./install.sh x86_64/goat-wallpapers-git/
-    ./install.sh x86_64/goat-applications-git/
-    ./install.sh x86_64/goat-icons-git/
-    ./install.sh x86_64/goat-junnunkarim-wallpapers-git/
-    ./install.sh x86_64/goat-mut-ex-wallpapers-git/
+BASE_DEPENDENCIES=(
+    archlinux-keyring
+    base-devel
+    git
+    vim
+    nushell
+    cargo
+    sddm
+    bspwm
+    sxhkd
+    alacritty
+    cronie
+    blueman
+    bluez
+    bluez-utils
+    bluez-libs
+    bluez-plugins
 )
 
-# Install dependencies
-nu -c 'paru -S (open $"($env.HOME)/pkgs.toml" | get pkgs.pacman.explicit.package | find --invert --regex "amtoine|wallpapers")'
+RAW_GOATFILES="https://raw.githubusercontent.com/goatfiles"
+REVISION="main"
 
-# Install dmenu-flexipatch
-git clone https://github.com/bakkeby/dmenu-flexipatch /tmp/dmenu
-cp $HOME/.config/dmenu-flexipatch/patches.h /tmp/dmenu
-(
-    cd /tmp/dmenu
-    sudo make clean install
+LOCAL_PKGS_FILE="/tmp/pkgs.toml"
+
+REMOTE_DMENU="https://github.com/bakkeby/dmenu-flexipatch"
+LOCAL_DMENU_DIR="/tmp/dmenu"
+
+REMOTE_PKGBUILDS="https://github.com/goatfiles/pkgbuilds"
+LOCAL_PKGBUILDS_DIR="/tmp/pkgbuilds"
+
+PKGBUILDS=(
+    goat-paru-git
+    goat-scripts-git
+    goat-sounds-git
+    goat-wallpapers-git
+    goat-applications-git
+    goat-icons-git
+    goat-junnunkarim-wallpapers-git
+    goat-mut-ex-wallpapers-git
 )
 
-# Activate useful system
-sudo systemctl enable sddm
-sudo systemctl enable NetworkManager
+SYSTEM_FILES=(
+    etc/pacman.conf
+    etc/pacman.d/mirrorlist
+    etc/sddm.conf
+    usr/share/sddm/themes/sugar-candy/theme.conf
+    var/spool/cron/user
+)
 
-# Greet
-echo 'Installation completed! Please reboot your computer.'
+SYSTEM_SERVICES=(
+    sddm
+    NetworkManager
+    cronie
+    bluetooth
+)
+
+REMOTE_DOTFILES="https://github.com/goatfiles/dotfiles"
+LOCAL_DOTFILES_DIR="$HOME/.local/share/ghq/github.com/goatfiles/dotfiles"
+
+
+echo_and_run () {
+    echo -e "\e[93m== running following command ==================\e[0m"
+    echo -e "\e[93m$@\e[0m"
+
+    eval "$@" || {
+        echo -e "\e[91m== an error occured... terminating... =========\e[0m"
+        echo -e "\e[91m'$@' failed...\e[0m"
+        echo -e "\e[91mcurrent pwd: $(pwd)\e[0m"
+        exit 1
+    }
+}
+
+
+install_base () {
+    echo_and_run sudo pacman --noconfirm -Syyu "${BASE_DEPENDENCIES[@]}"
+}
+
+
+synchronize_database () {
+    echo_and_run sudo pacman -Fy
+}
+
+
+install_pkgbuilds () {
+    # Clean and clone pkgbuilds
+    [ -d "$LOCAL_PKGBUILDS_DIR" ] && echo_and_run sudo rm -r "$LOCAL_PKGBUILDS_DIR"
+    echo_and_run git clone "$REMOTE_PKGBUILDS" "$LOCAL_PKGBUILDS_DIR"
+
+    # Build pkgbuilds
+    (
+        echo_and_run cd "$LOCAL_PKGBUILDS_DIR"
+        for pkgbuild in "${PKGBUILDS[@]}"; do
+            echo_and_run ./cleanup.sh
+            yes | echo_and_run ./install.sh "x86_64/$pkgbuild/PKGBUILD" || exit 1
+        done
+        echo_and_run ./cleanup.sh
+    )
+    [ "$?" -ne 0 ] && exit 1
+}
+
+
+install_dependencies () {
+    echo_and_run curl -fLo "$LOCAL_PKGS_FILE" "$RAW_GOATFILES/dotfiles/$REVISION/pkgs.toml"
+
+    pacman_deps=$(nu -c "\
+        open $LOCAL_PKGS_FILE \
+        | get pkgs.pacman.explicit.package \
+        | find --invert --regex 'goat-.*-git'\
+    ")
+    rust_deps=$(nu -c "\
+        open $LOCAL_PKGS_FILE \
+        | get pkgs.rust.cargo.package \
+    ")
+    python_deps=$(nu -c "\
+        open $LOCAL_PKGS_FILE \
+        | get pkgs.python.pip.package \
+    ")
+
+    echo_and_run paru -S $pacman_deps
+    echo_and_run cargo install $rust_deps
+    echo_and_run pip install $python_deps
+}
+
+
+install_dmenu () {
+    [ -d "$LOCAL_DMENU_DIR" ] && echo_and_run sudo rm -r "$LOCAL_DMENU_DIR"
+    echo_and_run git clone "$REMOTE_DMENU" "$LOCAL_DMENU_DIR"
+
+    echo_and_run curl -fLo "$LOCAL_DMENU_DIR/patches.h" "$RAW_GOATFILES/dotfiles/$REVISION/.config/dmenu-flexipatch/patches.h"
+
+    (
+        echo_and_run cd "$LOCAL_DMENU_DIR"
+        yes | echo_and_run sudo make clean install || exit 1
+    )
+    [ "$?" -ne 0 ] && exit 1
+}
+
+
+install_system_files () {
+    for file in "${SYSTEM_FILES[@]}"; do
+        echo_and_run sudo mkdir -p "/$(dirname "$file")"
+        echo_and_run sudo curl -fLo "/$file" "$RAW_GOATFILES/system/$REVISION/$file"
+    done
+}
+
+
+activate_system () {
+    for service in "${SYSTEM_SERVICES[@]}"; do
+        echo_and_run sudo systemctl enable "$service"
+    done
+}
+
+
+pull_dotfiles () {
+    echo_and_run git clone --bare "$REMOTE_DOTFILES" "$LOCAL_DOTFILES_DIR"
+    cfg="/usr/bin/git --git-dir=$LOCAL_DOTFILES_DIR --work-tree=$HOME"
+    echo_and_run $cfg reset --hard
+    echo_and_run $cfg config --local status.showUntrackedFiles no
+    echo_and_run $cfg config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    echo_and_run $cfg fetch
+    echo_and_run $cfg checkout "$REVISION"
+}
+
+
+install_base
+synchronize_database
+
+install_pkgbuilds
+#install_dependencies
+install_dmenu
+
+install_system_files
+activate_system
+
+pull_dotfiles
+
+echo -e "\e[92mInstallation completed! Please reboot your computer\e[0m."
 
