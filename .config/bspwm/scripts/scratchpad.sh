4b2e666c2f4e3c573732c97b143ca48bbd87ab7e
amtoine <stevan.antoine@gmail.com>
2023-07-21 18:29:30 +0200

    remove the Bash scripts

--- .config/bspwm/scripts/scratchpad.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-# only add floating scratchpad window node id to /tmp/scratchid
-bspc query -N -n .floating | xargs -I {} sh -c 'bspc query  --node {} -T | grep -q scratchpad && echo {} > /tmp/scratchid'
-exec $SHELL
