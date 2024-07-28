8c843a3d4a098c7669a055e3cf359cdf51e36e9c
amtoine <stevan.antoine@gmail.com>
2024-07-28 13:14:35 +0200

    remove example packages

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -7,13 +7,7 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   home.packages = [
-    pkgs.hello
-
     (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "Mononoki" "FiraCode" ]; })
-
-    (pkgs.writeShellScriptBin "my-hello" ''
-      echo "Hello, ${config.home.username}!"
-    '')
 
     pkgs.neovim
   ];
